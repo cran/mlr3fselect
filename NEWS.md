a8d22d456bd4af6f35d2a459c2ba2c7407538514
Marc Becker <marcbecker@posteo.de>
2020-10-22 20:20:02 +0000

    version 0.4.0

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,23 @@
+# mlr3fselector 0.4.0
+
+* Compact in-memory representation of R6 objects to save space when saving mlr3
+  objects via saveRDS(), serialize() etc.
+* `FSelectorRFE` supports fraction of features to retain in each iteration
+  (`feature_fraction`), number of features to remove in each iteration
+  (`feature_number`) and vector of number of features to retain in each 
+  iteration (`subset_sizes`).
+* `AutoFSelect` is renamed to `AutoFSelector`.
+* To retrieve the inner feature selection results in nested resampling,
+  `as.data.table(rr)$learner[[1]]$fselect_result` must be used now.
+* Option to control `store_benchmark_result`, `store_models` and `check_values`
+  in `AutoFSelector`. `store_fselect_instance` must be set as a parameter during 
+  initialization.
+* Adds `FSelectorGeneticSearch`.
+* Fixes `check_values` flag in `FSelectInstanceSingleCrit` and
+  `FSelectInstanceMultiCrit`.
+* Removed dependency on orphaned package `bibtex`.
+* `PipeOpSelect` is internally used for task subsetting.
+
 # mlr3fselect 0.3.0
 
 * `Archive` is `ArchiveFSelect` now which stores the benchmark result in
---
+++ R/FSelectorGeneticSearch.R
@@ -0,0 +1,82 @@
+#' @title Feature Selection via Genetic Search
+#'
+#' @description
+#' `FSelectorGeneticSearch` class that implements an Genetic Search. Calls
+#' [genalg::rbga.bin()] from package \CRANpkg{genalg}.
+#'
+#' @templateVar id genetic_search
+#' @template section_dictionary_fselectors
+#'
+#' @section Parameters:
+#' \describe{
+#' \item{`suggestions`}{`list()`}
+#' \item{`popSize`}{`integer(1)`}
+#' \item{`mutationChance`}{`numeric(1)`}
+#' \item{`elitism`}{`integer(1)`}
+#' \item{`zeroToOneRatio`}{`integer(1)`}
+#' \item{`iters`}{`integer(1)`}
+#' }
+#'
+#' For the meaning of the control parameters, see [genalg::rbga.bin()].
+#' [genalg::rbga.bin()] internally terminates after `iters` iteration. We set
+#' `ìters = 100000`  to allow the termination via our terminators. If more
+#' iterations are needed, set `ìters` to a higher value in the parameter set.
+#'
+#' @export
+#' @examples 
+#' library(mlr3)
+#'
+#' terminator = trm("evals", n_evals = 5)
+#'
+#' instance = FSelectInstanceSingleCrit$new(
+#'   task = tsk("iris"),
+#'   learner = lrn("classif.rpart"),
+#'   resampling = rsmp("holdout"),
+#'   measure = msr("classif.ce"),
+#'   terminator = terminator
+#' )
+#'
+#' fselector = fs("genetic_search", popSize = 10L)
+#'
+#' # Modifies the instance by reference
+#' fselector$optimize(instance)
+#'
+#' # Returns best scoring evaluation
+#' instance$result
+#'
+#' # Allows access of data.table of full path of all evaluations
+#' instance$archive$data()
+FSelectorGeneticSearch = R6Class("FSelectorGeneticSearch",
+  inherit = FSelector,
+  public = list(
+
+    #' @description
+    #' Creates a new instance of this [R6][R6::R6Class] class.
+    initialize = function() {
+      ps = ParamSet$new(list(
+        ParamUty$new("suggestions"),
+        ParamInt$new("popSize", lower = 5L, default = 200L),
+        ParamDbl$new("mutationChance", lower = 0, upper = 1),
+        ParamInt$new("elitism", lower = 1L),
+        ParamInt$new("zeroToOneRatio", lower = 1, default = 10L),
+        ParamInt$new("iters", lower = 1, default = 100000L)
+      ))
+      ps$values$iters = 100000L
+
+      super$initialize(param_set = ps, properties = "single-crit")
+    }
+  ),
+  private = list(
+    .optimize = function(inst) {
+      pars = self$param_set$values
+      if (is.null(pars$mutationChance)) pars$mutationChance = NA
+      if (is.null(pars$elitism)) pars$elitism = NA
+      n = inst$objective$domain$length
+
+      mlr3misc::invoke(genalg::rbga.bin, size = n,
+        evalFunc = inst$objective_function, .args = pars)
+    }
+  )
+)
+
+mlr_fselectors$add("genetic_search", FSelectorGeneticSearch)
